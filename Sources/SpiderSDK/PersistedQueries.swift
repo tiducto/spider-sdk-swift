7ce6882b0f8b1c581f0f901646703035f00ca05e
xplayerCZ <40500030+xplayerCZ@users.noreply.github.com>
2026-09-24 21:40:47 +0000

    chore(contract): sync SDK to spider-contract v0.6

--- Sources/SpiderSDK/PersistedQueries.swift
+++ Sources/SpiderSDK/PersistedQueries.swift
@@ -11,7 +11,7 @@ struct PersistedOp {
 
 enum PersistedQueries {
     static let departures = PersistedOp(id: "70a644fe3c6b2cbf5b2d70cef8230c1428bea6357ae1766772162d86469563d0", path: "departures")
-    static let plan = PersistedOp(id: "dad4f190af803a8cb50ec99c5852544297e94db8edc0d94220c8f79d98f065a7", path: "plan")
-    static let planstream = PersistedOp(id: "5a92b2943c4bdce61190cf8399277dacc8a5fb20e1caf9e760d3681598fe49b6", path: "plan-stream")
+    static let plan = PersistedOp(id: "06004d101213f2d6abbbde9e7ed3fd239af47352168d5fd47ece8c46cab67618", path: "plan")
+    static let planstream = PersistedOp(id: "7f82dbee066bcb53a1ddfe83254abb396c408c5ce95a151ac743c656789aef4b", path: "plan-stream")
     static let trip = PersistedOp(id: "e8959a8d47a8e8437ee3ec740cd9c3e28bd401efdd236dde0502559daea53920", path: "trip")
 }
